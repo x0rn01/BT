c9ffa8dd764c95061cef94e92eaf86a5ec42aa2b
Sébastien Wouters <sebastien.wouters@hotmail.com>
2019-08-21 16:31:55 +0200

    Branching: 9.3

--- git-commit.sh
+++ git-commit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git commit -m "[GRADLE RELEASE] - version update" --quiet
+git add gradle.properties
+git commit -m "$1"
+# git push -> won't work because of authentication
 git rev-parse HEAD
